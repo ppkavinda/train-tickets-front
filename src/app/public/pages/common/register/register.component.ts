11901f6f13ce6e5ca7d688a4d4218c6c3904d6c3
prasad kavinda <pp.kavinda@gmail.com>
2020-02-05 00:14:53 +0530

    fix register form

--- src/app/public/pages/common/register/register.component.ts
+++ src/app/public/pages/common/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  public registerForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
+
+  initForm() {
+    this.registerForm = new FormGroup({
+      firstName: new FormControl("", [Validators.required]),
+      lastName: new FormControl("", [Validators.required]),
+      mobile: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required])
+    });
   }
 
 }
