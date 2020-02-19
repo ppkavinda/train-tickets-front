e7a8229bb9516759fb5d2b05d5465d18620e6fc9
Prasad Kavinda <prasad.gunasekara@inovaitsys.com>
2020-02-19 19:40:59 +0530

    add endpoints to node server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+ 
+var bodyParser = require('body-parser');
+ 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/train-schedule'));
@@ -11,5 +16,71 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/train-schedule/index.html'));
 });
 
+
+app.get('/companies', (req, res) => {
+  const companies = [
+    { "id": 1, "name": "Company One" },
+    { "id": 2, "name": "Company Two" }
+  ]
+  return res.json({
+    companies: companies
+  })
+});
+
+app.post('/login', (req, res) => {
+  const user = {
+    "id": 1,
+    "firstname": "prasad",
+    "lastname": "kavinda",
+    "email": "pp.kaidvna@gmail.com",
+    "token": "Bearer asdlfkjasdlkfjsdfkljdasfkldjsafkljkljlksj"
+  }
+  return res.json({
+    user: user
+  })
+});
+
+app.post('/register', (req, res) => {
+  const user = {
+    "id": 1,
+    "firstname": "prasad",
+    "lastname": "kavinda",
+    "email": "pp.kaidvna@gmail.com",
+    "token": "Bearer asdlfkjasdlkfjsdfkljdasfkldjsafkljkljlksj"
+  }
+  return res.json({
+    user: user
+  })
+});
+
+app.post('/train/search', (req, res) => {
+  const trains = [
+    {
+      "id": 1,
+      "from": "colombo",
+      "to": "matara",
+      "classes":
+        [
+          { "2": { "price": 140, "name": "2nd class" } },
+          { "3": { "price": 70, "name": "3rd class" } }
+        ]
+    },
+    {
+      "id": 2,
+      "from": "maradana",
+      "to": "beliatta",
+      "classes":
+        [
+          { "2": { "price": 290, "name": "2nd class" } },
+          { "3": { "price": 180, "name": "3rd class" } }
+        ]
+    }
+  ]
+  return res.json({
+    trains: trains
+  })
+});
+
+
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 5000);
