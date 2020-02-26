65ce895a44d15ff6df3c097b83eaa661c7cacdf1
Prasad Kavinda <prasad.gunasekara@inovaitsys.com>
2020-02-26 09:11:44 +0530

    modify train-details page

--- src/app/dashboard/train-details/train-details.component.ts
+++ src/app/dashboard/train-details/train-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/public/services/auth.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { TrainService } from 'src/app/public/services/train.service';
 import * as M from "materialize-css/dist/js/materialize";
+import { AlertService } from 'src/app/public/services/alert.service';
 // import * as $ from 'jquery';
 
 declare var $: any;
@@ -17,15 +18,37 @@ export class TrainDetailsComponent implements OnInit {
   constructor(
     public authService: AuthService,
     private route: ActivatedRoute,
+    private router: Router,
     private trainService: TrainService,
+    private alertService: AlertService
   ) { }
 
   ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+        // console.log(params);
+      if (!params['to'] || !params['from']) {
+        
+        this.router.navigate(['dashboard']);
+        this.alertService.success('asdf', true);
+      }
+    })
     var from = document.querySelectorAll('#from');
     var to = document.querySelectorAll('#to');
     $(from).datepicker({dateFormat:'dd/mm/yy'});
     $(to).datepicker({dateFormat:'dd/mm/yy'});
     
+    var ticketCategory = document.querySelectorAll('#ticket-category');
+    $(ticketCategory).formSelect();
+
+    var trainName = document.querySelector('#train-name');
+    var ticketPrice = document.querySelector('#ticket-price');
+    $(trainName).focusin();
+    $(ticketPrice).focusin();
+
+    var fromLabel = document.querySelector('#from-label');
+    var toLabel = document.querySelector('#to-label');
+    $(fromLabel).focusin();
+    $(toLabel).focusin();
     // // M.Datepicker.init(from, null);
     // M.Datepicker.init(to, null);
   }
